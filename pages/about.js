8d766bb4e025335aa9f719beef0c03b9eb175e2b
Gareth Carter <garethcarter@MacBook-Pro-2.local>
2021-03-27 18:03:43 +0000

    adding additional styles for layout and typeface for body text

--- pages/about.js
+++ pages/about.js
@@ -16,8 +16,8 @@ export default function About() {
             <Layout>
                 <h1 className={styles.heading}>Hello.</h1>
                 <p>My name is Gareth Carter, and I’m a web developer based in London.</p>
-                <p>My career began in the archives and heritage sector, where I digitised and catalogued books and historic documents. Still, programming was never far from my mind: I studied the history of computing and logic at university, which inspired me to engage with the subject practically—scratching my itch for creative problem-solving.</p>
-                <p>Later on, I broadened my practical programming skill in order to streamline my digitisation and cataloguing work. Ultimately, I attended Le Wagon’s intensive web development bootcamp—as well as self-directed courses in Python and React—where I acquired the skills to match my curiosity and ambition.</p>
+                <p>My career began in the archives and heritage sector, where I digitised and catalogued books and historic documents. Still, programming was never far from my mind: I studied the history of computing and philosophical logic at university, and loved the way it scratched my itch for creative problem-solving.</p>
+                <p>Initially, I taught myself to code in order to streamline my digitisation and cataloguing work. Later, I attended Le Wagon’s intensive web development bootcamp—as well as self-directed learning with Python and React—where I acquired the skills to match my curiosity and ambition.</p>
                 <p>Now equipped with a robust skillset, I relish the opportunity to explore new technologies that improve how we engage with each other online. Combining creativity and curiosity, my aim is to build web applications that are beautiful, accessible, and performant.</p>
             </Layout>
         </>
