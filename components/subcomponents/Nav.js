64a33f6d23ea05ad8c04e130d993b0b3a97a1a58
Gareth Carter <garethcarter@MacBook-Pro-2.local>
2021-03-31 23:54:09 +0100

    adding functionality to  nav

--- components/subcomponents/Nav.js
+++ components/subcomponents/Nav.js
@@ -1,21 +1,27 @@
 import Link from 'next/link';
 import styles from '../../styles/Header.module.css'
+import { useRouter } from 'next/router';
 
 export default function Nav() {
+    const router = useRouter();
+    const style = {
+        borderBottom: "2px solid"
+    }
+    
     return (
         <nav className={styles.nav}>
             <ul className={styles.navList}>
-                <li className={styles.navListItem}>
+                <li className={styles.navListItem} style={router.pathname == "/about" ? style : null}>
                     <Link href="/about">
                         <a>about</a>
                     </Link>
                 </li>
-                <li className={styles.navListItem}>
+                <li className={styles.navListItem} style={router.pathname == "/work" ? style : null}>
                     <Link href="/work">
                         <a>work</a>
                     </Link>
                 </li>
-                <li className={styles.navListItem}>
+                <li className={styles.navListItem} style={router.pathname == "/contact" ? style : null}>
                     <Link href="/contact">
                         <a>contact</a>
                     </Link>
