6d652eafe0ef65ace56f79092f420f07015106f8
Gareth Carter <garethcarter@MacBook-Pro-2.local>
2021-04-04 23:41:24 +0100

    added new link security and styles for accessibility

--- pages/contact.js
+++ pages/contact.js
@@ -22,7 +22,7 @@ export default function Contact() {
                 />
             </div> */}
             <p>Have you got a question? Or perhaps you're interested in collaborating with me on a project? Feel free to <a className={styles.link} href="mailto:gareth.carter@outlook.com?subject=Hi!">get in touch</a>.</p>
-            <p>I can also be found on <a className={styles.link} href="https://github.com/gcarter89" target="_blank">Github</a> and <a className={styles.link} href="https://linkedin.com/in/gcarter89/" target="_blank">LinkedIn</a>.</p>
+            <p>I can also be found on <a className={styles.link} href="https://github.com/gcarter89" target="_blank" rel="noreferrer">Github</a> and <a className={styles.link} href="https://linkedin.com/in/gcarter89/" target="_blank" rel="noreferrer">LinkedIn</a>.</p>
         </>
     )
 
--- pages/work.js
+++ pages/work.js
@@ -21,7 +21,7 @@ export default function Work() {
                         src="/portfolio.png"
                         alt="Mock ups of portfolio website for laptop and mobile"
                         layout="responsive"
-                        height="58%"
+                        height="57%"
                         width="100%"
                         priority={'true'} 
                     />
@@ -34,7 +34,7 @@ export default function Work() {
                 <h3 className={styles.subheading}>Description</h3>
                 <p>This website <em>had</em> to be included as an example of my work. The underlying design philosophy for this website is "simple done well".</p>
                 <p>On a technical level, I wanted my website to do three things: First and foremost, I want the website to be accessible to all: that means making my HTML meaningful, well-structured, and well-signposted. Secondly, I wanted the website to load quickly and without intruding on the user's experience (in this case, getting information about me). Finally, I want my website to be easily findable on the web: this means, utilising good SEO standards for each page.</p>
-                <p><a>React</a> was chosen due to its ability to create a performant and easily manageable component-based UI. Supplementing React, this website utilises the <a>NextJS</a> framework. Given the advantages of built-in routing, siloed styling, Server Side Rendering (with its SEO benefits) the framework seems like a good foundation for my website.</p>
+                <p>React was chosen due to its ability to create a performant and easily manageable component-based UI. Supplementing React, this website utilises the NextJS framework. Given the advantages of built-in routing, siloed styling, Server Side Rendering (with its SEO benefits) the framework seems like a good foundation for my website.</p>
                 <p>I used <a href="https://developers.google.com/web/tools/lighthouse">Google's Lighthouse</a> tool to quantify and assess the performance and quality of my personal website.</p>
                 <p>In terms of design, I went for a straightforward, contrastive colour scheme (an off-white, with black and blue) that grabs attention where it wants without being intrusive. Ontop of the colour scheme, I opted for a font that evokes familiarity: Gill Sans is a typographical homage to the London Underground's <a href="https://en.wikipedia.org/wiki/Johnston_(typeface)">Johnston</a> typeface.</p>
         </>
