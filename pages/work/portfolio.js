cc9531351d62c4ed197dac33058ef23018896613
Gareth Carter <garethcarter@MacBook-Pro-2.local>
2021-04-06 18:07:29 +0100

    completed copy and styling for portfolio page

--- pages/work/portfolio.js
+++ pages/work/portfolio.js
@@ -4,6 +4,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Portfolio() {
+    const style = {
+        fontWeight: 400
+    }
     return (
         <>
         <Head>
@@ -28,12 +31,16 @@ export default function Portfolio() {
                 <li className={styles.listItem}>NextJS</li>
                 <li className={styles.listItem}>JavaScript</li>
             </ul>
-            <h2 className={styles.subheading}>Description</h2>
-            <p>This website <em>had</em> to be included as an example of my work. The underlying design philosophy for this website is "simple done well".</p>
-            <p>On a technical level, I wanted my website to do three things: First and foremost, I want the website to be accessible to all: that means making my HTML meaningful, well-structured, and well-signposted. Secondly, I wanted the website to load quickly and without intruding on the user's experience (in this case, getting information about me). Finally, I want my website to be easily findable on the web: this means, utilising good SEO standards for each page.</p>
-            <p>React was chosen due to its ability to create a performant and easily manageable component-based UI. Supplementing React, this website utilises the NextJS framework. Given the advantages of built-in routing, siloed styling, Server Side Rendering (with its SEO benefits) the framework seems like a good foundation for my website.</p>
-            <p>I used <a href="https://developers.google.com/web/tools/lighthouse">Google's Lighthouse</a> tool to quantify and assess the performance and quality of my personal website.</p>
-            <p>In terms of design, I went for a straightforward, contrastive colour scheme (an off-white, with black and blue) that grabs attention where it wants without being intrusive. Ontop of the colour scheme, I opted for a font that evokes familiarity: Gill Sans is a typographical homage to the London Underground's <a href="https://en.wikipedia.org/wiki/Johnston_(typeface)">Johnston</a> typeface.</p>
+            <h2 className={styles.subheading}>Summary</h2>
+            <p>My portfolio website <em>had</em> to be included as an example of my work because it exemplifies my approach to web development: <span style={style}>simple done well</span>. I wanted to convey this clearly in the technical and design decisions taken to create the application.</p>
+            <a href="https://github.com/gcarter89/portfolio" target="_blank" rel="noreferrer" className={styles.link}>View Repository »</a>
+            <h2 className={styles.subheading}>Technical</h2>
+            <p>I chose React to build the frontend of this application because it simplifies the process of building interactive, data-driven UI with its component-based approach. Being able to implement logic for discrete UI components across my application without directly manipulating the DOM made for a better developer experience.</p>
+            <p>As I look to the future, I believe React will furnish me with the flexibility to further develop this application. In future iterations, I will be looking to incorporate a more data-driven frontend—gradually implementing APIs and CMS. Currently, I think React will make it easier for me—from a developer experience perspective—to imbricate the frontend with data.</p>
+            <h2 className={styles.subheading}>Design</h2>
+            <p>On the design front, I decided to prioritise function over form.</p>
+            <p>The colour scheme forms the foundation for the website—composed of off-white, black and blue. Off-white was used to accentuate space on the page, whereas black and blue were employed to differentiate text and image according to function or emphasis.</p>
+            <p>Building on the colour scheme, I opted for a clean font that evokes familiarity: Gill Sans is a typographical homage to the Johnston typeface—which is associated with London Underground’s signage. Aside from being a call-back to the city where I live, I wanted the font to be simply, clean and geometric—in accord with the overarching theme of the website.</p>
     </>
     )
 }
