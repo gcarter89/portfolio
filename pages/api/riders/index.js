7e03cd944f88c72d3d4851700d93b214702af90e
Gareth Carter <gareth.carter@outlook.com>
2021-04-19 13:16:27 +0100

    added try catch to diagnose server problem

--- pages/api/riders/index.js
+++ pages/api/riders/index.js
@@ -3,29 +3,38 @@ const prisma = new PrismaClient()
 
 //add number of wins per classification
 export default async (request, response) => {
-    const riders = await prisma.rider.findMany({
-        include: {
-            _count: {
-                select: { 
-                    gcWins: true,
-                    komWins: true,
-                    pointsWins: true,
-                    intSprintWins: true,
-                    youngRiderWins: true,
-                    combinationWins: true
+    try {
+        const riders = await prisma.rider.findMany({
+            include: {
+                _count: {
+                    select: { 
+                        gcWins: true,
+                        komWins: true,
+                        pointsWins: true,
+                        intSprintWins: true,
+                        youngRiderWins: true,
+                        combinationWins: trsfue
+                    }
                 }
             }
-        }
-
-    })
-    
-    riders.forEach(rider => {
-        for (let prop in rider._count) {
-            if (rider._count[prop] === 0) {
-                rider._count[prop] = undefined
+        })
+        
+        riders.forEach(rider => {
+            for (let prop in rider._count) {
+                if (rider._count[prop] === 0) {
+                    rider._count[prop] = undefined
+                }
             }
-        }
-    })
-
-    response.status(200).json(riders);
+        })
+    
+        response.status(200).json(riders);
+        
+    } catch (error) {
+        // response.status(500).send('Internal Server Error');
+        console.error(error)
+        throw error;
+        
+        
+    }
+    
 }
